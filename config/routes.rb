4168a55bfae5627ea9823d4eac9020bb92bac228
Rob Conery <rob@conery.io>
2024-03-28 16:59:49 -0700

    Installed spina

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  
   resources :users
-  get '/about', to: 'home#about'
+  #get '/about', to: 'home#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  mount Spina::Engine => '/'
 end
