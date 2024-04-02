f4754685b895fad15b5b5059502f41de564dbaab
Rob Conery <rob@conery.io>
2024-04-01 17:05:17 -0700

    Lets gooooo

---
+++ db/dokku.sh
@@ -0,0 +1,3 @@
+#DATABASE_URL:  postgres://postgres:88181f0f8958e00c3d7b3b5c9c466219@dokku-postgres-railzdb:5432/railzdb
+psql postgres://postgres:88181f0f8958e00c3d7b3b5c9c466219@164.92.75.205:15617/railzdb < db.sql
+
